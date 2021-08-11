4997497a5c926579c294550e74d08f8a386b60cd
sandroScheuermann <sandroryze@gmail.com>
2021-08-11 17:02:19 -0300

    Banco para o veículo implementado.

---
+++ Rech-a-car/Database/dbo/Tables/TBVeiculos.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[TBVeiculos] (
+    [MODELO]        VARCHAR (50)    NOT NULL,
+    [MARCA]         VARCHAR (50)    NOT NULL,
+    [QUILOMETRAGEM] INT             NOT NULL,
+    [ANO]           INT             NOT NULL,
+    [PLACA]         VARCHAR (50)    NOT NULL,
+    [CAPACIDADE]    INT             NOT NULL,
+    [PORTAS]        INT             NOT NULL,
+    [CHASSI]        VARCHAR (50)    NOT NULL,
+    [PORTA_MALAS]   VARCHAR (50)    NOT NULL,
+    [AUTOMATICO]    BIT             NOT NULL,
+    [FOTO]          VARBINARY (MAX) NOT NULL,
+    [ID]            INT             NOT NULL
+);
+
