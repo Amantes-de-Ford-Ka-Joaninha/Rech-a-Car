c84a9c43fa3d68f400b74850534e3e3e1f396f6a
Arkhandyr <victor.hm2000@aluno.ifsc.edu.br>
2021-08-11 18:11:32 -0300

    help

--- Rech-a-car/WindowsApp/Program.cs
+++ Rech-a-car/WindowsApp/Program.cs
@@ -17,6 +17,7 @@ namespace WindowsApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Teste INFERNO
         }
     }
 }
