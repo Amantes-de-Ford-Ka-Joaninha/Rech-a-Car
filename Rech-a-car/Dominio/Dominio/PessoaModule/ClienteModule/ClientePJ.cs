9ba73a5d6917fa679d9728e801196a47efa10736
João Xavier <fastjonh@gmail.com>
2021-08-17 17:58:06 -0300

    ajeitando errinho condutores para motoristas

--- Rech-a-car/Dominio/Dominio/PessoaModule/ClienteModule/ClientePJ.cs
+++ Rech-a-car/Dominio/Dominio/PessoaModule/ClienteModule/ClientePJ.cs
@@ -4,13 +4,14 @@ namespace Dominio.PessoaModule.ClienteModule
 {
     public class ClientePJ : PessoaJuridica, ICliente
     {
-        public ClientePJ(string nome, string telefone, string endereco, string documento, List<Condutor> condutores)
+        public List<MotoristaEmpresa> Motoristas { get; set; }
+        public ClientePJ(string nome, string telefone, string endereco, string documento, List<MotoristaEmpresa> motoristas)
         {
             Nome = nome;
             Telefone = telefone;
             Endereco = endereco;
             Documento = documento;
-            Condutores = condutores;
+            Motoristas = motoristas;
         }
     }
 }
