8568bd6eac212aeb7a427e053043f0d8a65f4792
João Xavier <fastjonh@gmail.com>
2021-08-17 13:54:12 -0300

    mudanças GetNovaEntidae() como generic

--- Rech-a-car/Dominio/ServicoModule/Servico.cs
+++ Rech-a-car/Dominio/ServicoModule/Servico.cs
@@ -4,6 +4,16 @@ namespace Dominio.ServicoModule
 {
     public class Servico : Entidade
     {
+        public Servico(string nome, double taxa)
+        {
+            Nome = nome;
+            Taxa1 = taxa;
+        }
+
+        public string Nome { get; set; }
+        public string Taxa { get; set; }
+        public double Taxa1 { get; }
+
         public override string Validar()
         {
             throw new NotImplementedException();
