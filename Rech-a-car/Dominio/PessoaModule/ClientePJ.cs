c2b5402230ceda6b33f58460c557ec5ce0e48697
sandroScheuermann <sandroryze@gmail.com>
2021-08-17 16:06:51 -0300

    Commit.

--- Rech-a-car/Dominio/PessoaModule/ClientePJ.cs
+++ Rech-a-car/Dominio/PessoaModule/ClientePJ.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Dominio.PessoaModule
 {
     public class ClientePJ : PessoaJuridica, ICliente
     {
+        public ClientePJ(string nome, string telefone, string endereco, string documento, List<Condutor> condutores)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+            Documento = documento;
+            Condutores = condutores;
+        }
+
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Endereco { get; set; }
         public string Documento { get; set; }
+        public List<Condutor> Condutores { get; set; }
+
+        Regex validaTelefone = new Regex(@"\b[1-9]{2}[1-9]{9}\b");
 
         public override string Validar()
         {
-            throw new NotImplementedException();
+            string validador = string.Empty;
+
+            if (Nome == string.Empty)
+                validador = "O cliente necessita de um nome.\n";
+            if (!validaTelefone.IsMatch(Telefone))
+                validador = "Telefone inválido.\n";
+
         }
     }
 }
