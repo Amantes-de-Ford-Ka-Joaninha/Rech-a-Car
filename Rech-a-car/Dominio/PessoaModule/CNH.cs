ba8cd4f2778d520ddc6de88e86b3c987ad51ebcf
sandroScheuermann <sandroryze@gmail.com>
2021-08-17 15:45:46 -0300

    Implementando Classe CNH, enum para Tipo de CNH, e Condutor.

---
+++ Rech-a-car/Dominio/PessoaModule/CNH.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Dominio.PessoaModule
+{
+    public class CNH : Entidade
+    {
+        public string numeroCnh { get; set; }
+        
+        public TipoCNH tipoCnh { get; set; }
+
+        Regex ValidarCNH = new Regex(@"\b(?=.*\d)[A-Za-z0-9]{1,11}\b");
+
+        public override string Validar()
+        {
+            string validacao = String.Empty;
+            if (ValidarCNH.IsMatch(numeroCnh))
+                validacao = "CNH Inválida.\n";
+
+            return validacao;
+        }
+    }
+}
