7be87720863ea92842b73fd0561532bb124343e4
sandroScheuermann <sandroryze@gmail.com>
2021-08-18 17:05:03 -0300

    Implementando banco de dados exclusivos para testes.

---
+++ Rech-a-car/DatabaseTests/dbo/Tables/TBFuncionario.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBFuncionario] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]           VARCHAR (110) NOT NULL,
+    [CPF]            VARCHAR (14)  NOT NULL,
+    [DataNascimento] DATE          NULL,
+    [Endereço]       VARCHAR (200) NOT NULL,
+    [Telefone]       VARCHAR (15)  NOT NULL,
+    [Cargo]          VARCHAR (110) NULL
+);
+
---
+++ Rech-a-car/DatabaseTests/dbo/Tables/TBVeiculo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[TBVeiculo] (
+    [MODELO]      VARCHAR (50)    NOT NULL,
+    [MARCA]       VARCHAR (50)    NOT NULL,
+    [ANO]         INT             NOT NULL,
+    [PLACA]       VARCHAR (50)    NOT NULL,
+    [CAPACIDADE]  INT             NOT NULL,
+    [PORTAS]      INT             NOT NULL,
+    [CHASSI]      VARCHAR (50)    NOT NULL,
+    [PORTA_MALAS] VARCHAR (50)    NOT NULL,
+    [CATEGORIA]   VARCHAR (50)    NOT NULL,
+    [AUTOMATICO]  BIT             NOT NULL,
+    [FOTO]        VARBINARY (MAX) NOT NULL,
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_TBVeiculo] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
