26ce54be6c19035fe2ba8abe94993c62c5fcae74
sandroScheuermann <sandroryze@gmail.com>
2021-08-19 17:55:48 -0300

    Começando a implementar os testes do funcionário.

---
+++ Rech-a-car/Tests/Tests/FuncionarioModule/DominioFuncionario.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests.Tests.FuncionarioModule
+{
+    [TestClass]
+    public class DominioFuncionario
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
