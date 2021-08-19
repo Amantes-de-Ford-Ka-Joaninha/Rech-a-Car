10d193178a827cfbc2970425e168a95b50ac0ece
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-08-19 11:06:21 -0300

    Implementando tela cliente PF

--- Rech-a-car/WindowsApp/WindowsApp/ClienteModule/CadastrarClientePF.Designer.cs
+++ Rech-a-car/WindowsApp/WindowsApp/ClienteModule/CadastrarClientePF.Designer.cs
@@ -29,7 +29,177 @@ namespace WindowsApp.ClienteModule
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbTelefone = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbEndereco = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbCPF = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tbNascimento = new System.Windows.Forms.Label();
+            this.tbCNH = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbTipoCNH = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(216, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Cliente (PF)";
+            // 
+            // tbNome
+            // 
+            this.tbNome.AutoSize = true;
+            this.tbNome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNome.ForeColor = System.Drawing.Color.White;
+            this.tbNome.Location = new System.Drawing.Point(48, 180);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(53, 21);
+            this.tbNome.TabIndex = 1;
+            this.tbNome.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(107, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.AutoSize = true;
+            this.tbTelefone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefone.ForeColor = System.Drawing.Color.White;
+            this.tbTelefone.Location = new System.Drawing.Point(34, 226);
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(67, 21);
+            this.tbTelefone.TabIndex = 1;
+            this.tbTelefone.Text = "Telefone";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(107, 223);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // tbEndereco
+            // 
+            this.tbEndereco.AutoSize = true;
+            this.tbEndereco.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEndereco.ForeColor = System.Drawing.Color.White;
+            this.tbEndereco.Location = new System.Drawing.Point(27, 272);
+            this.tbEndereco.Name = "tbEndereco";
+            this.tbEndereco.Size = new System.Drawing.Size(74, 21);
+            this.tbEndereco.TabIndex = 1;
+            this.tbEndereco.Text = "Endereço";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(107, 269);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(204, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // tbCPF
+            // 
+            this.tbCPF.AutoSize = true;
+            this.tbCPF.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCPF.ForeColor = System.Drawing.Color.White;
+            this.tbCPF.Location = new System.Drawing.Point(383, 180);
+            this.tbCPF.Name = "tbCPF";
+            this.tbCPF.Size = new System.Drawing.Size(37, 21);
+            this.tbCPF.TabIndex = 1;
+            this.tbCPF.Text = "CPF";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(426, 177);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(211, 29);
+            this.textBox4.TabIndex = 4;
+            // 
+            // tbNascimento
+            // 
+            this.tbNascimento.AutoSize = true;
+            this.tbNascimento.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNascimento.ForeColor = System.Drawing.Color.White;
+            this.tbNascimento.Location = new System.Drawing.Point(327, 272);
+            this.tbNascimento.Name = "tbNascimento";
+            this.tbNascimento.Size = new System.Drawing.Size(93, 21);
+            this.tbNascimento.TabIndex = 1;
+            this.tbNascimento.Text = "Nascimento";
+            // 
+            // tbCNH
+            // 
+            this.tbCNH.AutoSize = true;
+            this.tbCNH.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCNH.ForeColor = System.Drawing.Color.White;
+            this.tbCNH.Location = new System.Drawing.Point(377, 226);
+            this.tbCNH.Name = "tbCNH";
+            this.tbCNH.Size = new System.Drawing.Size(43, 21);
+            this.tbCNH.TabIndex = 1;
+            this.tbCNH.Text = "CNH";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(426, 223);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(151, 29);
+            this.textBox6.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(426, 269);
+            this.maskedTextBox1.Mask = "00 / 00 / 0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(211, 29);
+            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::WindowsApp.Properties.Resources.adicionar;
+            this.button1.Location = new System.Drawing.Point(581, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 70);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cbTipoCNH
+            // 
+            this.cbTipoCNH.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTipoCNH.FormattingEnabled = true;
+            this.cbTipoCNH.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "AB",
+            "C",
+            "D",
+            "E"});
+            this.cbTipoCNH.Location = new System.Drawing.Point(581, 223);
+            this.cbTipoCNH.Name = "cbTipoCNH";
+            this.cbTipoCNH.Size = new System.Drawing.Size(56, 29);
+            this.cbTipoCNH.TabIndex = 6;
             // 
             // CadastrarClientePF
             // 
@@ -37,12 +207,44 @@ namespace WindowsApp.ClienteModule
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(33)))), ((int)(((byte)(34)))));
             this.ClientSize = new System.Drawing.Size(668, 522);
+            this.Controls.Add(this.cbTipoCNH);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbCPF);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbCNH);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tbNascimento);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.tbEndereco);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label1);
             this.Name = "CadastrarClientePF";
             this.Text = "CadastrarClientePF";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tbNome;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label tbTelefone;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label tbEndereco;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label tbCPF;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label tbNascimento;
+        private System.Windows.Forms.Label tbCNH;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbTipoCNH;
     }
 }
--- Rech-a-car/WindowsApp/WindowsApp/ClienteModule/CadastrarClientePF.cs
+++ Rech-a-car/WindowsApp/WindowsApp/ClienteModule/CadastrarClientePF.cs
@@ -1,8 +1,12 @@
 ﻿using Controladores.PessoaModule;
 using Controladores.Shared;
+using Dominio.PessoaModule;
 using Dominio.PessoaModule.ClienteModule;
 using System;
+using System.Globalization;
+using System.Windows.Forms;
 using WindowsApp.Shared;
+using WindowsApp.VeiculoModule;
 
 namespace WindowsApp.ClienteModule
 {
@@ -21,13 +25,27 @@ namespace WindowsApp.ClienteModule
         }
         public override ClientePF GetNovaEntidade()
         {
-            throw new NotImplementedException();
+            var nome = tbNome.Text;
+            var telefone = tbTelefone.Text;
+            var endereco = tbEndereco.Text;
+            var documento = tbCPF.Text;
+            var cnh = GetCNH();
+            DateTime dataNascimento = DateTime.ParseExact(tbNascimento.Text, "dd/MM/yyyy", CultureInfo.CreateSpecificCulture("pt-BR"));
+            return new ClientePF(nome, telefone, endereco, documento, cnh, dataNascimento);
+        }
+
+        public CNH GetCNH()
+        {
+            var numero = tbNome.Text;
+            var tipo = cbTipoCNH.SelectedIndex;
+
+            return new CNH(numero, (TipoCNH)tipo);
         }
 
         private void bt_adicionar_Click(object sender, EventArgs e)
         {
             Salva();
-            Close();
+            TelaPrincipal.Instancia.FormAtivo = new GerenciamentoVeiculo();
         }
     }
 }
