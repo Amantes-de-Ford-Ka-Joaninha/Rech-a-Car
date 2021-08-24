fc12ffb306241e9140a7aeb04d6f4dff16dd615b
sandroScheuermann <sandroryze@gmail.com>
2021-08-24 13:57:40 -0300

    commit p/ atualizar

--- Rech-a-car/Tests/Tests/FuncionarioModule/DominioFuncionario.cs
+++ Rech-a-car/Tests/Tests/FuncionarioModule/DominioFuncionario.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-
+using Dominio.PessoaModule.ClienteModule;
+using Dominio.PessoaModule;
+using FluentAssertions;
 namespace Tests.Tests.FuncionarioModule
 {
     [TestClass]
     public class DominioFuncionario
     {
+
+
+
+
         [TestMethod]
         public void TestMethod1()
         {
