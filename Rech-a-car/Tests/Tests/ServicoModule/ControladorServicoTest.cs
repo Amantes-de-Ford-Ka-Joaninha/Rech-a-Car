26abe6ebf508519f433c9393ff583c9d34d854f8
sandroScheuermann <sandroryze@gmail.com>
2021-08-24 14:16:05 -0300

    Criando base para o tests/Servico

---
+++ Rech-a-car/Tests/Tests/ServicoModule/ControladorServicoTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests.Tests.ServicoModule
+{
+    [TestClass]
+    public class ControladorServicoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Rech-a-car/Tests/Tests/ServicoModule/DominioServicoTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests.Tests.ServicoModule
+{
+    [TestClass]
+    public class DominioServicoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
