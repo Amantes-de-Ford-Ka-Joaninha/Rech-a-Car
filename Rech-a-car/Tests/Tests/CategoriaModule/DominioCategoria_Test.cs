2a4d7e2e456d47fc6e1414aa289450624728068b
sandroScheuermann <sandroryze@gmail.com>
2021-08-27 17:39:39 -0300

    implementando testes categoria

---
+++ Rech-a-car/Tests/Tests/CategoriaModule/DominioCategoria_Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Dominio.VeiculoModule;
+using Dominio.PessoaModule;
+using FluentAssertions;
+
+namespace Tests.Tests.CategoriaModule
+{
+    [TestClass]
+    public class DominioCategoria_Test
+    {
+        Grupo grupo;
+
+
+        [TestMethod]
+        public void Deve_retornar_grupo_valido()
+        {
+            grupo = new Grupo("nome", 10, 10, 10, 10, TipoCNH.A);
+            grupo.Validar().Should().Be(string.Empty);
+        }
+        [TestMethod]
+        public void Deve_retornar_grupo_invalido()
+        {
+            grupo = new Grupo("", -10, -10, -10, -10, TipoCNH.A);
+            grupo.Validar().Should().NotBe(string.Empty);
+        }
+    }
+}
