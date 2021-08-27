4981d02d326f8a47c48419639d0cfeb7ec8ffff3
sandroScheuermann <sandroryze@gmail.com>
2021-08-27 14:27:18 -0300

    Adicionando campo NOME no TBDadosVeículo, renomeando para TBCategoria

---
+++ Rech-a-car/Database/dbo/Tables/TBCategoria.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TBCategoria] (
+    [ID]            INT          NOT NULL,
+    [QUILOMETRAGEM] INT          NOT NULL,
+    [PRECO_KM]      DECIMAL (18) NOT NULL,
+    [DIARIA]        DECIMAL (18) NOT NULL,
+    [NOME]          VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_TBCategoria] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_TBCategoria_TBVeiculo] FOREIGN KEY ([ID]) REFERENCES [dbo].[TBVeiculo] ([ID]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+
