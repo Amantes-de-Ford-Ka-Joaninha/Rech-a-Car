a5dc50fb0d4d3626be3212902b64632be39077fb
sandroScheuermann <sandroryze@gmail.com>
2021-08-30 12:41:39 -0300

    Corrigind configurareditar

--- Rech-a-car/WindowsApp/WindowsApp/AluguelModule/ResumoAluguel.cs
+++ Rech-a-car/WindowsApp/WindowsApp/AluguelModule/ResumoAluguel.cs
@@ -17,23 +17,9 @@ namespace WindowsApp.AluguelModule
 
         public override Aluguel GetNovaEntidade()
         {
-            throw new System.NotImplementedException();
-        }
-
-        protected override IEditavel ConfigurarEditar()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public ResumoAluguel()
-        {
-            InitializeComponent();
-        }
-        protected void ConfigurarTextFields()
-        {
             tbCliente.Text = entidade.Cliente.ToString();
             tbDocumento.Text = entidade.Cliente.Documento;
-            tbEndereço.Text = entidade.Cliente.Endereco; 
+            tbEndereço.Text = entidade.Cliente.Endereco;
             tbTelefone.Text = entidade.Cliente.Telefone;
             tbMarca.Text = entidade.Veiculo.Marca;
             tbModelo.Text = entidade.Veiculo.Modelo;
@@ -42,6 +28,30 @@ namespace WindowsApp.AluguelModule
             tbCondutor.Text = entidade.Condutor.Nome;
             tbDt_Emprestimo.Text = entidade.DataAluguel.ToString("d");
             listOpcionais.DataSource = entidade.Servicos;
+
+            return new Aluguel();
         }
+
+        protected override IEditavel ConfigurarEditar()
+        {
+            tbCliente.Text = entidade.Cliente.ToString();
+            tbDocumento.Text = entidade.Cliente.Documento;
+            tbEndereço.Text = entidade.Cliente.Endereco;
+            tbTelefone.Text = entidade.Cliente.Telefone;
+            tbMarca.Text = entidade.Veiculo.Marca;
+            tbModelo.Text = entidade.Veiculo.Modelo;
+            tbPlaca.Text = entidade.Veiculo.Placa;
+            tbPlano.SelectedItem = entidade.TipoPlano.ToString();
+            tbCondutor.Text = entidade.Condutor.Nome;
+            tbDt_Emprestimo.Text = entidade.DataAluguel.ToString("d");
+            listOpcionais.DataSource = entidade.Servicos;
+            return this;
+        }
+
+        public ResumoAluguel()
+        {
+            InitializeComponent();
+        }
+
     }
 }
