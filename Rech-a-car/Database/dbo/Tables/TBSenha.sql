37b6743150f811589a68434853f708d47fa89f1f
João Xavier <fastjonh@gmail.com>
2021-08-30 03:38:18 -0300

    sim

--- Rech-a-car/Database/dbo/Tables/TBSenha.sql
+++ Rech-a-car/Database/dbo/Tables/TBSenha.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[TBSenha] (
-    [ID]             INT             NOT NULL,
+    [ID]             INT             IDENTITY (1, 1) NOT NULL,
     [ID_FUNCIONARIO] INT             NOT NULL,
     [HASH_SENHA]     VARCHAR (MAX)   NOT NULL,
     [SALT]           VARBINARY (MAX) NOT NULL,
     CONSTRAINT [PK_TBSenha] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_TBSenha_TBFuncionario] FOREIGN KEY ([ID_FUNCIONARIO]) REFERENCES [dbo].[TBFuncionario] ([ID])
 );
+
+
 
 
 
