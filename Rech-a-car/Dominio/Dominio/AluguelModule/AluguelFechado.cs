bf10f1223022568a882a98a3177f5ad8cd180176
sandroScheuermann <sandroryze@gmail.com>
2021-09-02 17:10:29 -0300

    Implementando os calculos do aluguel.

--- Rech-a-car/Dominio/Dominio/AluguelModule/AluguelFechado.cs
+++ Rech-a-car/Dominio/Dominio/AluguelModule/AluguelFechado.cs
@@ -6,7 +6,7 @@ namespace Dominio.AluguelModule
 {
     public class AluguelFechado : Aluguel
     {
-        public AluguelFechado(Aluguel aluguel, int kmRodados, double tanqueUtilizado, List<Servico> servicosNecessarios, DateTime dataDevolvida) : base(aluguel)
+        public AluguelFechado(Aluguel aluguel, int kmRodados, double tanqueUtilizado, List<Servico> servicosNecessarios) : base(aluguel)
         {
             Veiculo = aluguel.Veiculo;
             Servicos = aluguel.Servicos;
@@ -17,7 +17,8 @@ namespace Dominio.AluguelModule
             KmRodados = kmRodados;
             TanqueUtilizado = tanqueUtilizado;
             ServicosNecessarios = servicosNecessarios;
-            DataDevolvida = dataDevolvida;
+            DataDevolvida = DateTime.Now;
+            DataDevolucao = aluguel.DataDevolucao;
         }
         public int KmRodados { get; set; }
         public double TanqueUtilizado { get; set; }
@@ -25,9 +26,54 @@ namespace Dominio.AluguelModule
         public DateTime DataDevolvida { get; set; }
         public double CalcularTotal()
         {
-            if (TipoPlano == Plano.diario)
-                return 0;
-            return 0;
+            double PrecoFinal = 0;
+            Servicos.ForEach(x => PrecoFinal += x.Taxa);
+
+            var Categoria = Veiculo.Categoria;
+
+            switch (TipoPlano.ToString())
+            {
+                case "diario":
+                    CalculaPlanoDiario();
+                    break;
+                case "controlado":
+                    CalculaPlanoControlado();
+                    break;
+                case "livre":
+                    CalculaPlanoLivre();
+                    break;
+                default:
+                    break;
+            }
+
+            void CalculaPlanoControlado()
+            {
+                PrecoFinal += (Categoria.PrecoDiaria * GetQtdDiasAluguel()) +
+                    Categoria.QuilometragemFranquia * Categoria.PrecoKm;
+            }
+            void CalculaPlanoDiario()
+            {
+                PrecoFinal += Categoria.PrecoDiaria * GetQtdDiasAluguel();
+            }
+            void CalculaPlanoLivre()
+            {
+                PrecoFinal += Categoria.PrecoDiaria * GetQtdDiasAluguel() * 1.3;
+            }
+            int GetQtdDiasAluguel()
+            {
+                return (DataDevolucao - DataAluguel).Days;
+            }
+
+            int diasAtraso = (DataDevolucao - DateTime.Now).Days;
+
+            PrecoFinal += diasAtraso * 50;
+
+            //CALCULAR GASOLINA DEPOIS.
+
+            ServicosNecessarios.ForEach(x => PrecoFinal += x.Taxa);
+
+            return PrecoFinal;
+
         }
     }
 }
--- Rech-a-car/WindowsApp/WindowsApp/AluguelModule/FechamentoAluguel.cs
+++ Rech-a-car/WindowsApp/WindowsApp/AluguelModule/FechamentoAluguel.cs
@@ -1,46 +1,117 @@
 ﻿using Dominio.AluguelModule;
 using Dominio.Shared;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using WindowsApp.Shared;
+using Dominio.ServicoModule;
+using Controladores.AluguelModule;
+using Controladores.VeiculoModule;
 
 namespace WindowsApp.AluguelModule
 {
     public partial class FechamentoAluguel : Form, IVisualizavel
     {
         private bool estadoBotoes = true;
+        Aluguel aluguel;
+        List<Servico> despesas = new List<Servico>();
+        ControladorAluguel controladorAluguel = new ControladorAluguel();
+        ControladorVeiculo controladorVeiculo = new ControladorVeiculo();
+        AluguelFechado aluguelFechado;
 
-        public FechamentoAluguel()
+        public FechamentoAluguel(Aluguel aluguel)
         {
+            this.aluguel = aluguel;
             InitializeComponent();
         }
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (estadoBotoes == true)
-            {
-                for (int i = 0; i < listChecklist1.Items.Count; i++)
-                {
-                    listChecklist1.SetItemChecked(i, estadoBotoes);
-                    listChecklist2.SetItemChecked(i, estadoBotoes);
-                }
-                checkBox1.Text = "Desmarcar todas as opções";
-            }
-            else
-            {
-                for (int i = 0; i < listChecklist1.Items.Count; i++)
-                {
-                    listChecklist1.SetItemChecked(i, estadoBotoes);
-                    listChecklist2.SetItemChecked(i, estadoBotoes);
-                }
-                checkBox1.Text = "Marcar todas as opções";
-            }
-            
-            estadoBotoes = !estadoBotoes;
-        }
+
+        //private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    if (estadoBotoes == true)
+        //    {
+        //        for (int i = 0; i < listChecklist1.Items.Count; i++)
+        //        {
+        //            listChecklist1.SetItemChecked(i, estadoBotoes);
+        //            listChecklist2.SetItemChecked(i, estadoBotoes);
+        //        }
+        //        checkBox1.Text = "Desmarcar todas as opções";
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i < listChecklist1.Items.Count; i++)
+        //        {
+        //            listChecklist1.SetItemChecked(i, estadoBotoes);
+        //            listChecklist2.SetItemChecked(i, estadoBotoes);
+        //        }
+        //        checkBox1.Text = "Marcar todas as opções";
+        //    }
+
+        //    estadoBotoes = !estadoBotoes;
+        //}
 
         public Form Visualizar<T>(T t) where T : IControlavel
         {
-            throw new NotImplementedException();        //deve preencher os textbox com os dados necessarios e retornar this
+            tb_KmInicial.Text = aluguel.Veiculo.Quilometragem.ToString();
+            return this;
         }
+
+        #region eventos
+
+        private void bt_AddDespesa_Click(object sender, EventArgs e)
+        {
+            if (tb_NomeDespesa.Text != "" && mtb_PrecoDespesa.Text != "" && double.TryParse(mtb_PrecoDespesa.Text, out double precoDespesa))
+                despesas.Add(new Servico(tb_NomeDespesa.Text, precoDespesa));
+
+            PopulaListDespesas();
+            lbValor.Text =  aluguelFechado.CalcularTotal().ToString();
+        }
+        private void bt_RemoveDespesa_Click(object sender, EventArgs e)
+        {
+            despesas.Remove((Servico)listDespesas.SelectedItem);
+            PopulaListDespesas();
+            lbValor.Text = aluguelFechado.CalcularTotal().ToString();
+        }
+        private void tb_Atraso_Leave(object sender, EventArgs e)
+        {
+            PopulaListDespesas();
+            lbValor.Text = aluguelFechado.CalcularTotal().ToString();
+        }
+        private void tb_KmFinal_TextChanged(object sender, EventArgs e)
+        {
+            aluguelFechado = aluguel.Fechar(CalcularKmRodado(), 0, despesas);
+            lbValor.Text = aluguelFechado.CalcularTotal().ToString();
+        }
+        private void tb_KmFinal_Leave(object sender, EventArgs e)
+        {
+            aluguelFechado = aluguel.Fechar(CalcularKmRodado(), 0, despesas);
+            lbValor.Text = aluguelFechado.CalcularTotal().ToString();
+        }
+        private void btFecharAluguel_Click(object sender, EventArgs e)
+        {
+            aluguelFechado = aluguel.Fechar(CalcularKmRodado(), 0, despesas);
+            controladorAluguel.Editar(aluguel.Id, aluguelFechado);
+            controladorVeiculo.Editar(aluguel.Veiculo.Id, aluguel.Veiculo);
+            AtualizarOdometro();
+        }
+        #endregion
+        private void PopulaListDespesas()
+        {
+            listDespesas.Items.Clear();
+            listDespesas.Items.AddRange(despesas.ToArray());
+        }
+        private int CalcularKmRodado()
+        {
+            int kmRodados = 0;
+            if (int.TryParse(tb_KmFinal.Text, out int odometroFinal) && odometroFinal > aluguel.Veiculo.Quilometragem)
+            {
+                kmRodados = odometroFinal - aluguel.Veiculo.Quilometragem;
+            }
+            return kmRodados;
+        }
+        private void AtualizarOdometro()
+        {
+            aluguel.Veiculo.Quilometragem += CalcularKmRodado();
+        }
+
     }
 }
